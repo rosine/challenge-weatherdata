1825b58ed3f4c7595f5aa10d779317b79affed26
Bettina <bettinaweller@web.de>
2018-07-02 10:59:24 +0200

    An Interface for a parser

---
+++ src/main/java/de/exxcellent/challenge/Parser.java
@@ -0,0 +1,13 @@
+package de.exxcellent.challenge;
+
+import java.util.ArrayList;
+
+/**
+ * An interface for parsers.
+ * @author Bettina Weller
+ *
+ */
+public interface Parser {
+
+	public String[] parse(String s);
+}
