96d0d9e1c40cca2b43a7059565740011b7921943
Bettina <bettinaweller@web.de>
2018-07-03 00:07:34 +0200

    Programme finds day with smallest spread

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -10,12 +10,8 @@ public final class App {
 
     public static void main(String... args) {
 
-        // Your preparation code …
-
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        String teamWithSmallesGoalSpread = "A good team"; // Your goal analysis function call …
-
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallesGoalSpread);
+        Controller controller = new Controller();
+        controller.getSmallestSpread("src\\main\\resources\\de\\exxcellent\\challenge\\weather.csv");
+        
     }
 }
