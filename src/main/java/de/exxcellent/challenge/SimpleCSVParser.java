a66dadbee2a08360c8a20f2f2b99baa46bdd17f4
Bettina <bettinaweller@web.de>
2018-07-02 11:01:37 +0200

    A simple parser vor csv format + tests
    SimpleCSVParser parses csv formatted strings.
    SipleCSVParserTest provides basic tests for SimpleCSVParser.

---
+++ src/main/java/de/exxcellent/challenge/SimpleCSVParser.java
@@ -0,0 +1,30 @@
+package de.exxcellent.challenge;
+
+/**
+ * A simple parser for csv input.
+ * @author Bettina Weller
+ *
+ */
+public class SimpleCSVParser implements Parser {
+
+	private String delimiter;
+	
+	public SimpleCSVParser() {
+		// set default delimiter for csv
+		delimiter = ",";
+	}
+	
+	@Override
+	public String[] parse(String s) {
+		String[] parsed = s.split(delimiter);
+		return parsed;
+	}
+	
+	public String getDelimiter() {
+		return delimiter;
+	}
+	
+	public void setDelimiter(String s) {
+		delimiter = s;
+	}
+}
---
+++ src/test/java/de/exxcellent/challenge/SimpleCSVParserTest.java
@@ -0,0 +1,78 @@
+package de.exxcellent.challenge;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimpleCSVParserTest {
+
+	private SimpleCSVParser parser;
+	private String parse;
+	private String[] compare;
+	
+	/**
+	 * Set up before tests.
+	 */
+	@Before
+	public void setUp() {
+		parser = new SimpleCSVParser();
+		System.out.println("Set up completed");
+	}
+	
+	/**
+	 * Tear down after tests.
+	 */
+	@After
+	public void tearDown() {
+		parser = null;
+		setParse("");
+		System.out.println("Tear down completed");
+	}
+	
+	/**
+	 * Parse a csv formatted string.
+	 */
+	@Test
+	public void parse() {
+		setParse("a,b,c");
+		setCompare(new String[] {"a", "b", "c"});
+		String[] parsed = parser.parse(parse);
+		
+		for(int i = 0; i < parsed.length; i++) {
+			assertEquals(parsed[i], compare[i]);
+		}
+	}
+
+	/**
+	 * Parse an empty string.
+	 */
+	@Test
+	public void parseEmpty() {
+		setParse("");
+		setCompare(new String[] {""});
+		String[] parsed = parser.parse(parse);
+		
+		for(int i = 0; i < parsed.length; i++) {
+			assertEquals(parsed[i], compare[i]);
+		}
+	}
+	
+	public String getParse() {
+		return parse;
+	}
+
+	public void setParse(String parse) {
+		this.parse = parse;
+	}
+
+	public String[] getCompare() {
+		return compare;
+	}
+
+	public void setCompare(String[] compare) {
+		this.compare = compare;
+	}
+
+}
